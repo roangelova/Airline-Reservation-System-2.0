ef4a9506a89df5a30786e9a6f77cbb9c6c41e74c
roangelova <roangelova97@gmail.com>
2022-08-26 17:17:08 +0300

    add enums

---
+++ AirlineReservationSystem.2.0/API/ARS.Common/Enums/AirlineType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARS.Common.Enums
+{
+    public enum AirlineType
+    {
+        LowCost,
+        NationalCarrier,
+        Network,
+        Private,
+        UltraLowCost
+    }
+}
---
+++ AirlineReservationSystem.2.0/API/ARS.Common/Enums/BaggageSize.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARS.Common.Enums
+{
+    public enum BaggageSize
+    {
+        Small,
+        Medium,
+        Large
+    }
+}
