e413271d1a2a5cef5878f2925f5daf834a04e152
roangelova <raangelova97@gmail.com>
2022-08-31 17:55:59 +0300

    add gradient styling to header

--- AirlineReservationSystem.2.0/React FE/ars/src/components/shared/Header.js
+++ AirlineReservationSystem.2.0/React FE/ars/src/components/shared/Header.js
@@ -2,18 +2,20 @@ import React from "react"
 
 const Header = () => {
     return (
-        <div className="ui large secondary inverted pointing menu">
+        <div className="cutomGradient">
+        <div className="ui large secondary pointing menu ">
             <a className="toc item">
                 <i className="plane icon" ></i>
             </a>
             <a className="active item">Home</a>
             <a className="item">Explore</a>
-            <a className="item">Company</a>
+            <a className="item">Book</a>
             <a className="item">Privilege Club</a>
             <div className="right item">
                 <a className="ui inverted button">Log in</a>
                 <a className="ui inverted button">Sign Up</a>
             </div>
+        </div>
         </div>
     )
 }
