3e943a754968e99bf440a7667f6ee953c2bd6ee0
roangelova <raangelova97@gmail.com>
2022-09-02 17:59:20 +0300

    include latest

--- AirlineReservationSystem.2.0/React FE/ars/src/App.js
+++ AirlineReservationSystem.2.0/React FE/ars/src/App.js
@@ -1,10 +1,12 @@
 import './assets/css/App.css';
+import Explore from './components/customer/Explore';
 import Header from './components/shared/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Explore/>
     </div>
   );
 }
