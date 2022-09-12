cfe5e98a630c14bef9000b553a678007611abe29
roangelova <raangelova97@gmail.com>
2022-09-12 14:54:36 +0300

    add pricing section

---
+++ AirlineReservationSystem.2.0/API/ARS.Persistance/Seed/DbInitializer.cs
@@ -0,0 +1,25 @@
+﻿using ARS.Common.Entities;
+using ARS.Persistance.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARS.Persistance.Seed
+{
+    public static class  DbInitializer
+    {
+        public static void SeedInitialData(ApplicationDbContext context)
+        {
+            if (context.Destinations.Any())
+            {
+                return;
+            }
+
+            var destinations = new List<Destination>();
+            var pathToJon = Path.Combine(Path.GetFullPath)
+
+        }
+    }
+}
