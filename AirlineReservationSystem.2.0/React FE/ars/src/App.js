1ce7f9229bece806fcdc504a638e7942f85bbd47
roangelova <raangelova97@gmail.com>
2022-10-20 17:54:47 +0300

    upload latest

--- AirlineReservationSystem.2.0/React FE/ars/src/App.js
+++ AirlineReservationSystem.2.0/React FE/ars/src/App.js
@@ -1,10 +1,12 @@
 import './assets/css/App.css';
 import Hero from './components/common/Hero';
+import Header from './components/shared/Header';
 
 function App() {
   return (
     <>
-        <Hero/>
+      <Header />
+      <Hero />
     </>
   );
 }
