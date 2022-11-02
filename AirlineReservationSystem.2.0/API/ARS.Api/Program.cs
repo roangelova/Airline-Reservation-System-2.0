54d8dabf78356bbd86c729d3e2aaa16513a5c24d
roangelova <raangelova97@gmail.com>
2022-11-02 11:51:58 +0200

    add token config to Progam

--- AirlineReservationSystem.2.0/API/ARS.Api/Program.cs
+++ AirlineReservationSystem.2.0/API/ARS.Api/Program.cs
@@ -1,9 +1,14 @@
+using System.Text;
+
 using ARS.Api.Middlewares;
 using ARS.Api.ServiceExtensions;
 using ARS.Common.Configurations;
 using ARS.Common.Entities;
 using ARS.Persistance.Context;
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,7 +20,6 @@ var jwtConfig = builder.Configuration.GetSection("Jwt");
 builder.Services.Configure<JwtTokenConfigurationModel>(jwtConfig);
 
 
-
 builder.Services.AddIdentityCore<User>(options =>
 {
     options.User.RequireUniqueEmail = true;
@@ -24,7 +28,21 @@ builder.Services.AddIdentityCore<User>(options =>
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
 
-builder.Services.AddAuthentication();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                 .AddJwtBearer(options =>
+                 {
+                     options.TokenValidationParameters = new TokenValidationParameters
+                     {
+                         ValidateIssuer = true,
+                         ValidateAudience = true,
+                         ValidateLifetime = true,
+                         ValidateIssuerSigningKey = true,
+                         ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                         ValidAudience = builder.Configuration["Jwt:Audience"],
+                         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+                     };
+                 });
+
 builder.Services.AddAuthorization();
 
 
@@ -41,6 +59,30 @@ builder.Services.AddSwaggerGen(c =>
         Title = "ARS",
         Version = "v2"
     });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+            new OpenApiSecurityScheme {
+                Reference = new OpenApiReference {
+                    Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new string[] {}
+        }
+    });
+
 });
 
 var app = builder.Build();
