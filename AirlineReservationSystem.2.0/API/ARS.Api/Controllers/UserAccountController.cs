5aa7e5025284b23a26b154f34e09c1e2854b7d50
roangelova <raangelova97@gmail.com>
2022-11-02 17:18:30 +0200

    add latest

--- AirlineReservationSystem.2.0/API/ARS.Api/Controllers/UserAccountController.cs
+++ AirlineReservationSystem.2.0/API/ARS.Api/Controllers/UserAccountController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ARS.Api.Controllers.BaseControllers;
+using ARS.Common.DTOs.User;
 
 namespace ARS.Api.Controllers
 {
@@ -8,5 +9,12 @@ namespace ARS.Api.Controllers
     [ApiController]
     public class UserAccountController : BaseApiController
     {
+
+        [HttpPost]
+        public async Task<ActionResult> Register(RegisterUserDTO registerUserDto)
+        {
+            Console.WriteLine(registerUserDto);
+            return Ok();
+        }
     }
 }
--- AirlineReservationSystem.2.0/API/ARS.Service/Contracts/IUserService.cs
+++ AirlineReservationSystem.2.0/API/ARS.Service/Contracts/IUserService.cs
@@ -4,10 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using ARS.Common.DTOs.User;
+using ARS.Common.Entities;
+
 namespace ARS.Service.Contracts
 {
     public interface IUserService
     {
+        Task<User> RegisterUser(RegisterUserDTO registerDTO);
         Task<bool> CheckUserExists(string userId);
     }
 }
