25dabd2d11b9c82e84bd6d1dcc9fd9a446a73611
roangelova <raangelova97@gmail.com>
2022-12-01 17:13:52 +0200

    fix node & node-sass incompatibility issue; start working on the Login Form; set some default css styles; add util css; change primary colors

--- AirlineReservationSystem.2.0/React FE/ars/src/App.tsx
+++ AirlineReservationSystem.2.0/React FE/ars/src/App.tsx
@@ -3,11 +3,12 @@ import './assets/css/style.css';
 import Header from './components/shared/Header';
 import Hero from 'components/common/Hero';
 import AdminLayout from 'components/admin/AdminLayout';
+import LoginForm from 'components/authorization/LoginForm';
 
 function App() {
   return (
     <div>
-     <AdminLayout/>
+     <LoginForm/>
     </div>
   );
 }
---
+++ AirlineReservationSystem.2.0/React FE/ars/style_defaults.md
@@ -0,0 +1,11 @@
+Font styles: 
+10 / 12 / 14 / 16 / 18 / 20 / 24 / 30 / 36 / 44 / 52 / 62 / 74 / 86 / 98
+
+Spacing system in px: 
+2 / 4 / 8 / 12 / 16 / 24 / 32 / 48 / 64 / 80 / 96 / 128
+
+Font weight: 
+Default: 400
+Medium: 500
+Semi-bold: 600
+Bold: 700
